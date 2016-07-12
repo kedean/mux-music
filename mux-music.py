5455919db06d6fdccc0734fed8a873a99242f066
Kevin Dean <kevinedean@gmail.com>
2016-07-11 21:56:52 -0500

    cleaning up main file slightly

--- mux-music.py
+++ mux-music.py
@@ -6,24 +6,27 @@ from flask import Flask
 
 mux = Flask(__name__)
 
-@mux.route("/")
-def hello():
-    return "Hello World!"
-
+#
+# Handles all requests for now
+# In the future this will become a delegator that determines what apps have been loaded and dispatches dynamically
+#
 @mux.route("/<app>/<action>")
-def pause(app, action):
+def handleAll(app, action):
     status = 200
     
-    if action == "pause":
-        pianobar.pause()
-    elif action == "resume":
-        pianobar.resume()
-    elif action == "next":
-        pianobar.next()
+    if app == "pianobar":    
+        if action == "pause":
+            pianobar.pause()
+        elif action == "resume":
+            pianobar.resume()
+        elif action == "next":
+            pianobar.next()
+        else:
+            status = 404 
     else:
-        status = 404 
+        status = 404
 
-    return json.dumps({"status":status, "action":action})    
+    return json.dumps({"status":status, "app":app, "action":action})
 
 if __name__ == "__main__":
     mux.run(host="0.0.0.0", port=8000)
